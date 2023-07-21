dbef9cbe6066252c6ebd33a703d31da67d18e38f
SNEHA BALU NATHE <SNEHA BALU NATHE@LAPTOP-SARV66TK>
2023-07-21 11:44:13 +0530

    First commit

---
+++ dayone/src/com/met/introduction/CompilerInput.java
@@ -0,0 +1,18 @@
+package com.met.introduction;
+import java.util.Scanner;
+
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value of the string");
+		String str1=sc.next();
+		String str2=sc.next();
+		System.out.println("The concatination of the strings"+ str1+str2);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
