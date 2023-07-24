9d6c4afe7808a0ec3875caa43eb469c1faa49f08
SNEHA BALU NATHE <SNEHA BALU NATHE@LAPTOP-SARV66TK>
2023-07-24 22:32:13 +0530

    Task is done for today

---
+++ dayone/src/org/tnsif/Loops/Dowhiletask2.java
@@ -0,0 +1,25 @@
+package org.tnsif.Loops;
+import java.util.Scanner;
+
+public class Dowhiletask2 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Number:");
+		int num=sc.nextInt();
+		int count=0;
+		do
+		{
+			int last_digit=num%10;
+			System.out.println(last_digit+" ");
+			num/=10;
+			count++;
+		 
+		}while(num>0);
+		
+		System.out.println("The no of digits in the Given number is:"+" "+count);
+
+	}
+
+}
